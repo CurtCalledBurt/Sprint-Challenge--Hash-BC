4040b6ee7bf4d16512e7cf027c7006d400ea2fcd
CurtCalledBurt <CurtCalledBurt@users.noreply.github.com>
2020-04-10 09:45:37 -0700

    finished ex2 and cleaned up ex1

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,19 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
-
-    pass
+    first_airport = "NONE"
+    # insert the tickets into the hashtable with source-destination as the key-value pair
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+        # find and save the first airport we need to go to
+        if ticket.source == "NONE":
+            first_airport = ticket.destination
+    
+    current_airport = first_airport
+    for i in range(len(route)):
+        # put the current_airport in the route array
+        route[i] = current_airport
+        # get the next place we are going
+        current_airport = hash_table_retrieve(hashtable, current_airport)
+    # return all but the last element of route as that is still None
+    return route[:-1]
